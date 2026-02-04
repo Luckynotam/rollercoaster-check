c87cdd9bee29281edf5a556466901e8a612dc8d7
99999 <arthuryellowman@gmail.com>
2026-02-04 09:53:55 +0100

    Put int around input and removed line 22 and line 23

--- main.py
+++ main.py
@@ -17,11 +17,9 @@ while running:
     #Get inputs
     os.system('cls')
     print("Rollercoaster-check™")
-    age = input("Voer leeftijd in: ")
+    age = int(input("Voer leeftijd in: "))
     height = input("Voer lengte in: ")
-    age = int(age)
-    height = int(height)
-
+    
     #Process checks
     if(age > age_check and height > height_check):
         os.system('cls')
